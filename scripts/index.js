58b4dcbd7c160199f5037fb588ece4a340c6c8de
Svetl Kim <sm512649@gmail.com>
2025-01-27 00:45:46 +0300

    Проект Место

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,36 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const container = document.querySelector('.content');
+const cardContainer = container.querySelector('.places__list');
+
+initialCards.forEach(addCard);
+
+function addCard (element) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__description').textContent = element.name;
+
+    deleteButton.addEventListener('click', cardsDelete);
+
+    cardContainer.append(cardElement);  
+};
+
+function cardsDelete (e){
+    let cardItem = container.querySelector('.card__delete-button').closest(".card");
+    cardItem.remove();
+};
+
+    
+ 
+   
+  
+
+ 
+  
+
+
